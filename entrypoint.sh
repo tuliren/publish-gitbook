870790e44e2a60df4f3a9d9c36edb26d33338a15
tuliren <tuliren@users.noreply.github.com>
2020-01-25 09:54:51 +0000

    Update gitbook 2020-01-25 09:54:51

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,41 @@
 #!/bin/sh -l
 
+print_info(){
+  echo "\033[32mINFO \033[0m $@" > /dev/stderr
+}
+
+print_error(){
+  echo "\033[31mERROR\033[0m \a $@" > /dev/stderr
+}
+
+PUBLISHER_REPO=""
+# setup publisher
+if [ -n "${INPUT_PERSONAL_TOKEN}" ]; then
+    print_info "using provided PERSONAL_TOKEN"
+    PUBLISHER_REPO="https://x-access-token:${INPUT_PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+elif [ -n "${INPUT_GITHUB_TOKEN}" ]; then
+    print_info "using automatic GITHUB_TOKEN"
+    PUBLISHER_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+else
+    print_error "no PERSONAL_TOKEN or GITHUB_TOKEN found, you can provide one in your workflow YAML"
+    exit 1
+fi
+
 # config git
 git config --local user.name "${GITHUB_ACTOR}"
 git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-# check github token
-[ -z "${INPUT_GITHUB_TOKEN}" ] && {
-  echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
-  exit 1;
-};
+# install gitbook
+print_info "installing gitbook-cli"
+npm install gitbook-cli  -g
+
+print_info "installing gitbook plugins"
+gitbook --version
+gitbook install
+
+# build gitbook
+print_info "buildling gitbook"
+gitbook build
 
 # checkout gh-pages branch
 set +e
@@ -17,18 +44,11 @@ git fetch origin && git reset origin/gh-pages
 git status
 set -e
 
-# install gitbook
-npm install gitbook-cli  -g
-gitbook -v
-gitbook install
-
-# build gitbook
-gitbook build
-
 # copy the static site files into the current directory
 cp -R _book/* .
 
 # remove 'node_modules' and '_book' directory
+print_info "cleaning artifacts"
 git clean -fx node_modules
 git clean -fx _book
 
@@ -39,9 +59,8 @@ git add .
 COMMIT_MESSAGE="Update gitbook `date '+%Y-%m-%d %H:%M:%S'`"
 git commit -a -m "${COMMIT_MESSAGE}"
 
-# setup publisher
-PUBLISHER_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git remote add publisher ${PUBLISHER_REPO}
 
 # push to the publisher
-git push -q -u publisher gh-pages
+print_info "pushing to gh-pages branch"
+git push -u publisher gh-pages
